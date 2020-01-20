129f0625496cd26f6348dd821bd5dad6b076af09
Tom Taylor <hello@tomtaylor.name>
2020-01-20 20:31:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = { schemas: [] };
+module.exports = { 
+  schemas: {
+    article: require('./src/core/schema/article.schema.json'),
+    block: require('./src/core/schema/block.schema.json'),
+    component: require('./src/core/schema/component.schema.json'),
+    config: require('./src/core/schema/config.schema.json'),
+    contentobject: require('./src/core/schema/contentobject.schema.json'),
+    course: require('./src/core/schema/course.schema.json')
+  }
+};
